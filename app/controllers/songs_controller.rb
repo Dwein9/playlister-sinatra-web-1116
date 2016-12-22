e5211491d81fa6a6fd83ebdd40bddc1ac1c81c00
David Weinstein <txea@Davids-MacBook-Air.local>
2016-12-21 22:20:03 -0500

    donee

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,8 @@
-class SongsController < ApplicationController 
+require 'rack-flash'
+class SongsController < ApplicationController
+enable :sessions
+use Rack::Flash
+
 
   get '/songs' do
     @songs = Song.all
@@ -6,7 +10,7 @@ class SongsController < ApplicationController
   end
 
   get '/songs/new' do
-    @genres = Genre.all 
+    @genres = Genre.all
     erb :'/songs/new'
   end
 
@@ -20,13 +24,26 @@ class SongsController < ApplicationController
     @artist = Artist.find_or_create_by(params[:artist])
     @song.artist = @artist
     @song.save
-    if !params[:genre].nil?
+    if !params[:genre]["name"].nil?
       @genre = Genre.find_or_create_by(params[:genre])
       @song.genres << @genre
     end
+    flash[:message] = "Successfully created song."
     redirect to "/songs/#{@song.slug}"
   end
 
+  get "/songs/:slug/edit" do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
 
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.save
+    flash[:message] = "Successfully updated song."
+    redirect("/songs/#{@song.slug}")
+  end
 
 end
